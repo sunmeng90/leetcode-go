b76cf2c22fcc6fad3d6cc3bcdddab344d5badcb7
Meng <sunmeng90@126.com>
2019-12-22 08:32:37 +0800

    p00107-binary-tree-level-order-traversal-ii traversal by level using queue 1. enqueue node at current level 2. iterate nodes in queue and add all children, and remove nodes in current level from head

---
+++ problems/p00107-binary-tree-level-order-traversal-ii/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"container/list"
+	"github.com/sunmeng90/leetcode-go/model"
+)
+
+/*
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+		3
+	   / \
+	  9  20
+		/  \
+	   15   7
+return its bottom-up level order traversal as:
+	[
+	  [15,7],
+	  [9,20],
+	  [3]
+	]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrderBottom(root *model.TreeNode) [][]int {
+	queue := list.New()
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+	queue.PushBack(root) // interface value added
+	for queue.Len() > 0 {
+		nodesCountInLevel := queue.Len()
+		nodes := make([]int, 0)
+		for i := 0; i < nodesCountInLevel; i++ { // add children for all nodes in current level
+			node := queue.Remove(queue.Front()).(*model.TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+			nodes = append(nodes, node.Val) // after add its children, then add current node to the result
+		}
+		result = append(result, nodes)
+	}
+	reverseSlice(result)
+	return result
+}
+
+func reverseSlice(data [][]int) {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+}

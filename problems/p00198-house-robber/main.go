a4972a51728b21988729e54061955c1d4aa2a127
Meng <sunmeng90@126.com>
2020-01-01 22:17:40 +0800

    p00198-house-robber 1. recursion -- Time Limit Exceeded 2. save computed result in memo, avoid time limitation issue 3. iterative way

--- problems/p00198-house-robber/main.go
+++ problems/p00198-house-robber/main.go
@@ -41,8 +41,21 @@ func robAt(nums []int, i int, memo []int) int {
 	return memo[i]
 }
 
+// iterative way
+func rob2(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	memo := make([]int, len(nums)+1)
+	memo[0], memo[1] = 0, nums[0]
+	for i := 1; i < len(nums); i++ {
+		memo[i+1] = util.Max(memo[i], memo[i-1]+nums[i])
+	}
+	return memo[len(nums)]
+}
+
 func main() {
-	println(rob([]int{114, 117, 207, 117, 235, 82, 90, 67, 143, 146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236, 81,
+	println(rob2([]int{114, 117, 207, 117, 235, 82, 90, 67, 143, 146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236, 81,
 		90, 222, 160, 165, 195, 187, 199, 114, 235, 197, 187, 69, 129, 64, 214, 228, 78, 188, 67, 205, 94, 205, 169,
 		241, 202, 144, 240}))
 }

6f5c80c5db1054763e83b2bd6f99b80f57219f6e
Meng <sunmeng90@126.com>
2020-01-02 22:32:13 +0800

    p00225-implement-stack-using-queues

---
+++ problems/p00225-implement-stack-using-queues/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "container/list"
+
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Example:
+
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+Notes:
+
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is
+empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque
+(double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+*/
+type MyStack struct {
+	data *list.List
+}
+
+/** Initialize your data structure here. */
+func Constructor() MyStack {
+	return MyStack{list.New()}
+}
+
+/** Push element x onto stack. */
+func (this *MyStack) Push(x int) {
+	this.data.PushBack(x)
+	for i := 0; i < this.data.Len()-1; i++ {
+		front := this.data.Front()
+		this.data.PushBack(front.Value)
+		this.data.Remove(front)
+	}
+}
+
+/** Removes the element on top of the stack and returns that element. */
+func (this *MyStack) Pop() int {
+	return this.data.Remove(this.data.Front()).(int) // interface to value
+}
+
+/** Get the top element. */
+func (this *MyStack) Top() int {
+	return this.data.Front().Value.(int) // interface to value
+
+}
+
+/** Returns whether the stack is empty. */
+func (this *MyStack) Empty() bool {
+	return this.data.Len() == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */

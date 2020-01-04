8ae9ad8f7c706994eccc60e64a2c4df026c2600b
Meng <sunmeng90@126.com>
2020-01-04 17:25:28 +0800

    p00204-count-primes

---
+++ problems/p00204-count-primes/main.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+Count the number of prime numbers less than a non-negative number, n.
+
+Example:
+
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+*/
+// TODO: not easy to understand
+func countPrimes(n int) int {
+	notPrime := make([]bool, n)
+	count := 0
+	for i := 2; i < n; i++ {
+		if notPrime[i] == false {
+			count++
+			for j := 2; i*j < n; j++ {
+				notPrime[i*j] = true
+			}
+		}
+	}
+	return count
+}

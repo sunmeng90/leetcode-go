64339de25e2e1acbc46d3f6b0664563d3925dab6
Meng <sunmeng90@126.com>
2020-01-05 09:22:04 +0800

    p00268-missing-number

---
+++ problems/p00268-missing-number/main.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+*/
+
+func missingNumber(nums []int) int {
+	expectedSum := (len(nums) + 1) * len(nums) / 2
+	for i := 0; i < len(nums); i++ {
+		expectedSum -= nums[i]
+	}
+	return expectedSum
+}

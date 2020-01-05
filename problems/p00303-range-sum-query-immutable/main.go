875dcdae7c301a85ed630aea6e4945703646e52f
Meng <sunmeng90@126.com>
2020-01-05 23:10:50 +0800

    p00303-range-sum-query-immutable

---
+++ problems/p00303-range-sum-query-immutable/main.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+Note:
+You may assume that the array does not change.
+There are many calls to sumRange function.
+*/
+
+type NumArray struct {
+	nums []int
+}
+
+func Constructor(nums []int) NumArray {
+	return NumArray{nums: nums}
+}
+
+func (this *NumArray) SumRange(i int, j int) int {
+	sum := 0
+	for ; i <= j; i++ {
+		sum += this.nums[i]
+	}
+	return sum
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(i,j);
+ */

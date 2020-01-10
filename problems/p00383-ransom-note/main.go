8ffdbe97a12c70eb7edd6744b4d6d465f9a89753
Meng <sunmeng90@126.com>
2020-01-11 00:01:30 +0800

    p00383-ransom-note

---
+++ problems/p00383-ransom-note/main.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function
+that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+
+Each letter in the magazine string can only be used once in your ransom note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+*/
+func canConstruct(ransomNote string, magazine string) bool {
+	charMap := make([]int, 26)
+	for i := 0; i < len(ransomNote); i++ {
+		charMap[ransomNote[i]-'a']++
+	}
+	for i := 0; i < len(magazine); i++ {
+		charMap[magazine[i]-'a']--
+	}
+	for i := 0; i < len(charMap); i++ {
+		if charMap[i] > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+}

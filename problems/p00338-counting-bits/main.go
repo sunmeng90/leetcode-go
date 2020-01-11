7c45839ccb964635fc2c79703c1265b6a273a686
Meng <sunmeng90@126.com>
2020-01-11 11:31:16 +0800

    p00338-counting-bits
    medium

---
+++ problems/p00338-counting-bits/main.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in
+their binary representation and return them as an array.
+
+Example 1:
+
+Input: 2
+Output: [0,1,1]
+Example 2:
+
+Input: 5
+Output: [0,1,1,2,1,2]
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+*/
+// @Medium
+func countBits(num int) []int {
+	result := make([]int, num+1)
+	for i := 0; i <= num; i++ {
+		result[i] = countBitsForNum(i)
+	}
+	return result
+}
+
+func countBitsForNum(num int) int {
+	count := 0
+	for num != 0 {
+		count++
+		num &= num - 1
+	}
+	return count
+}
+
+func main() {
+
+}

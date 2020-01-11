ba0bdf259e1a91e0fb279606635e7e5a497c2c00
Meng <sunmeng90@126.com>
2020-01-11 10:42:47 +0800

    p00389-find-the-difference
    xor

--- problems/p00389-find-the-difference/main.go
+++ problems/p00389-find-the-difference/main.go
@@ -33,6 +33,19 @@ func findTheDifference(s string, t string) byte {
 	}
 	return 0
 }
+
+// XOR
+func findTheDifference2(s string, t string) byte {
+	result := byte(0)
+	for i := 0; i < len(s); i++ {
+		result ^= s[i]
+	}
+	for i := 0; i < len(t); i++ {
+		result ^= t[i]
+	}
+	return result
+}
+
 func main() {
 
 }
